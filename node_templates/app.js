3ac5b6e26f1d2c270cfe6e42d06a802c2f452422
Roberto Maldonado <rbt.maldonado@gmail.com>
2024-05-22 11:05:12 -0500

    Relocated filepath updated

--- node_templates/app.js
+++ node_templates/app.js
@@ -3,56 +3,37 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+// Register in middleware the parsing of the body
 app.use(express.urlencoded({ extended: true }));
 
+// Register in middleware the static files
+app.use(express.static("public"));
+
+// Routes for the application
 app.get("/", (req, res) => {
-  let htmlFilePath = path.join(
-    __dirname,
-    "frontend-site",
-    "views",
-    "index.html"
-  );
+  let htmlFilePath = path.join(__dirname, "views", "index.html");
   res.sendFile(htmlFilePath);
 });
 
 app.get("/about", (req, res) => {
-  let htmlFilePath = path.join(
-    __dirname,
-    "frontend-site",
-    "views",
-    "about.html"
-  );
+  let htmlFilePath = path.join(__dirname, "views", "about.html");
   res.sendFile(htmlFilePath);
 });
 
 app.get("/confirm", (req, res) => {
-  let htmlFilePath = path.join(
-    __dirname,
-    "frontend-site",
-    "views",
-    "confirm.html"
-  );
+  let htmlFilePath = path.join(__dirname, "views", "confirm.html");
   res.sendFile(htmlFilePath);
 });
 
 app.get("/recommend", (req, res) => {
-  let htmlFilePath = path.join(
-    __dirname,
-    "frontend-site",
-    "views",
-    "recommend.html"
-  );
+  let htmlFilePath = path.join(__dirname, "views", "recommend.html");
   res.sendFile(htmlFilePath);
 });
 
 app.get("/restaurants", (req, res) => {
-  let htmlFilePath = path.join(
-    __dirname,
-    "frontend-site",
-    "views",
-    "restaurants.html"
-  );
+  let htmlFilePath = path.join(__dirname, "views", "restaurants.html");
   res.sendFile(htmlFilePath);
 });
 
+// Start the server
 app.listen(port);
