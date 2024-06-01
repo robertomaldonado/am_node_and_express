e1bf3a2dfadb28d403bf0d75923af2770d39315d
Roberto Maldonado <rbt.maldonado@gmail.com>
2024-05-31 21:20:01 -0500

    Updated styles and fixed router bug

--- working_with_mysql/routes/blog.js
+++ working_with_mysql/routes/blog.js
@@ -2,4 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.redirect("/posts");
+});
+
+router.get("/posts", (req, res) => {
+  res.render("posts-list");
+});
+
 module.exports = router;
