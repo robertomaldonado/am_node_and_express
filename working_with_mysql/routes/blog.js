fb25bc000f21310039d663faa1dc31ba85ea4d4f
Roberto Maldonado <rbt.maldonado@gmail.com>
2024-06-04 22:01:04 -0500

    Added data fetched from db

--- working_with_mysql/routes/blog.js
+++ working_with_mysql/routes/blog.js
@@ -8,8 +8,13 @@ router.get("/", (req, res) => {
   res.redirect("/posts");
 });
 
-router.get("/posts", (req, res) => {
-  res.render("posts-list");
+router.get("/posts", async (req, res) => {
+  const query = `
+    SELECT posts.*, authors.name AS author_name FROM posts
+    INNER JOIN authors ON posts.author_id = authors.id
+  `;
+  const [posts] = await db.query(query);
+  res.render("posts-list", { posts: posts });
 });
 
 router.get("/new-post", async (req, res) => {
