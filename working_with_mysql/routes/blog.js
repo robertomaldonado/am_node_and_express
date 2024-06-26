c0da1602d2e146fd10f2f6d30614131ebee13f29
Roberto Maldonado <rbt.maldonado@gmail.com>
2024-06-26 14:11:09 -0500

    Detail page added

--- working_with_mysql/routes/blog.js
+++ working_with_mysql/routes/blog.js
@@ -8,8 +8,37 @@ router.get("/", (req, res) => {
   res.redirect("/posts");
 });
 
+router.get("/posts/:id", async (req, res) => {
+  const query = `
+  SELECT posts.*, authors.name AS author_name, authors.email AS author_email
+  FROM posts 
+    INNER JOIN authors ON posts.author_id = authors.id 
+  WHERE posts.id = ${req.params.id}`;
+  [posts] = await db.query(query);
+  // Handle no posts
+  if (!posts || posts.length === 0) {
+    res.status(404).render("404");
+    return;
+  }
+  const postData = {
+    ...posts[0],
+    date: posts[0].date.toISOString(),
+    humandReadableDate: posts[0].date.toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }),
+  };
+
+  res.render("post-detail", { post: postData });
+});
+
 router.get("/posts", async (req, res) => {
-  const query = `SELECT posts.*, authors.name AS author_name FROM posts INNER JOIN authors ON posts.author_id = authors.id`;
+  const query = `
+  SELECT posts.*, authors.name AS author_name 
+  FROM posts
+    INNER JOIN authors ON posts.author_id = authors.id`;
   const [posts] = await db.query(query);
   res.render("posts-list", { posts: posts });
 });
